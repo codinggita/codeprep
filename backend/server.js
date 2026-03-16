40a13be53da2d85831dabb698101d4d6d35e401b
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-03-16 11:52:32 +0530

    update base files

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,12 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
-// const dashboardRoutes = require("./routes/dashboardRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
+const problemRoutes = require("./routes/problemRoutes");
+const revisionRoutes = require("./routes/revisionRoutes");
+const goalsRoutes = require("./routes/goalsRoutes");
+const analyticsRoutes = require("./routes/analyticsRoutes");
+const profileRoutes = require("./routes/profileRoutes");
 
 require("./cron/syncProblems");
 
@@ -20,6 +24,11 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/problems", problemRoutes);
+app.use("/api/revision", revisionRoutes);
+app.use("/api/goals", goalsRoutes);
+app.use("/api/analytics", analyticsRoutes);
+app.use("/api/profile", profileRoutes);
 
 const PORT = process.env.PORT || 5000;
 
