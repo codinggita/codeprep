71911fd5aaa7f0a856bc543ff2fc4aea9e29e73b
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2026-03-16 13:59:17 +0530

    update basic files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AppRoutes from "./routes/AppRoutes";
+
 function App() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        CodePrep Frontend Ready 🚀
-      </h1>
-    </div>
-  )
+  return <AppRoutes />;
 }
 
 export default App
